74ecdf696459e50c381ca0182c67095d8ec4a349
Gary Yu <gairy.yu@gmail.com>
2019-06-16 17:07:21 +0800

    put GrinBridge into ios project

--- react-native-wallet/App.js
+++ react-native-wallet/App.js
@@ -3,9 +3,14 @@ import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import createStore from './app/reducers'
 import AppNavigation from './app/navigation/AppNavigation'
+import * as RNFS from 'react-native-fs'
+import {NativeModules} from "react-native"
 
 export default class App extends Component {
   configureStore = createStore()
+
+  var grinBridge = NativeModules.GrinBridge.init(false, ${RNFS.DocumentDirectoryPath} + '/wallet/', 'my-password')
+  grinBridge.walletRecovery('survey write again earth song palace wreck adjust genre upper arctic episode wonder resource actress eye aspect few element fiscal announce note sword early')
 
   render() {
     return (
